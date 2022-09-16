1c098c8259afd59f18b7c800a3ccf55de18ce28f
rahalh <hamzarahal031@gmail.com>
2022-09-16 14:45:24 +0100

    fixes version conflict error

--- udagram-api-user/src/server.ts
+++ udagram-api-user/src/server.ts
@@ -37,7 +37,7 @@ import {V0_USER_MODELS} from './controllers/v0/model.index';
   app.use('/api/v0/', IndexRouter);
 
   // Root URI call
-  app.get( '/', async ( req, res ) => {
+  app.get( '/', async (req: any , res: any) => {
     res.send( '/api/v0/' );
   } );
 
